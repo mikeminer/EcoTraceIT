6314159647081673fed93b5a4e4eecfa287a813c
mikeminer <4n0nym0u5.cyber@gmail.com>
2026-07-16 08:27:53 +0200

    chore: capture pre-hydration document shape

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,6 +2,20 @@ import {StrictMode, startTransition} from "react";
 import {hydrateRoot} from "react-dom/client";
 import {HydratedRouter} from "react-router/dom";
 
+const elementShape = (element: Element | null) => element ? {
+  attributes: [...element.attributes].map((attribute) => attribute.name),
+  children: [...element.children].map((child) => ({
+    tag: child.tagName,
+    attributes: [...child.attributes].map((attribute) => attribute.name),
+  })),
+} : null;
+
+console.error("EcoTraceIT prehydrate shape", JSON.stringify({
+  html: elementShape(document.documentElement),
+  head: elementShape(document.head),
+  body: elementShape(document.body),
+}));
+
 startTransition(() => {
   hydrateRoot(
     document,
